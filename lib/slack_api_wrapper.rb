5b9190deb1baddb7dc6b79d0b68d08031b8580a5
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-30 15:58:10 -0700

    refactored into class method channel and helper method in slackapi wrapper and refactoring of views affected into helper method/attr_reader rather than hash access defined by Slack

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -11,10 +11,14 @@ class SlackApiWrapper
     data = HTTParty.get(url)
 
     if data["channels"]
-      return data["channels"]
+      channel_list = data["channels"].map do |channel|
+        create_channel(channel_data)
+      end
     else
       return []
     end
+
+    return channel_list
   end
 
   def self.send_message(channel, message)
@@ -25,7 +29,7 @@ class SlackApiWrapper
 
     #.success comews with HTTParty
 
-  response =  HTTParty.post(url,
+    response =  HTTParty.post(url,
       body:  {
         "text" => "#{message}",
         "channel" => "#{channel}",
@@ -36,5 +40,19 @@ class SlackApiWrapper
       :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' }
     )
     return response.success?
+  end
+
+  private
+
+  def self.create_channel(api_params)
+    return Channel.new(
+      api_params["name"],
+      api_params["id"],
+      {
+        purpose: api_params["purpose"],
+        is_archived: api_params["is_archived"],
+        members: api_params["members"]
+      }
+    )
   end
 end
