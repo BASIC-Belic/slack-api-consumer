786dcd7dca8d35ff7e6f3c3dda339a3b9faf8ab0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-30 14:54:17 -0700

    self.list_channels method

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -2,4 +2,19 @@ require 'httparty'
 
 class SlackApiWrapper
   # Your code here!
+  BASE_URL = "https://slack.com/api/"
+  TOKEN = ENV["SLACK_TOKEN"]
+
+  def self.list_channels
+    url = BASE_URL + "channels.list?" + "token=#{TOKEN}" + "&exclude_archived=1"
+
+    data = HTTParty.get(url)
+
+    if data["channels"]
+      return data["channels"]
+    else
+      return []
+    end
+  end
+  
 end
