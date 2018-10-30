976f5fe8c6972f7342c1dcd23291b31a7a3a3ed8
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-30 15:32:16 -0700

    SlackApiWrapper.send_message

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -16,5 +16,25 @@ class SlackApiWrapper
       return []
     end
   end
-  
+
+  def self.send_message(channel, message)
+    url = BASE_URL + "chat.postMessage?" + "token=#{TOKEN}"
+
+    #why a hash? can write anything here
+    #HTTP wants hash with body and headers/args when doing post
+
+    #.success comews with HTTParty
+
+  response =  HTTParty.post(url,
+      body:  {
+        "text" => "#{message}",
+        "channel" => "#{channel}",
+        "username" => "Roberts-Robit",
+        "icon_emoji" => ":robot_face:",
+        "as_user" => "false"
+      },
+      :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+    return response.success?
+  end
 end
