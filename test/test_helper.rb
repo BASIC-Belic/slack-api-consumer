3cdbcab2db0828f80a1c8eedd7127efb0aebde86
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-31 11:02:21 -0700

    after configured VCR, new directory we defined in config test/cassettes/ and channel.yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,10 @@ require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 require 'slack_api_wrapper'
 
+require 'vcr'
+require 'webmock/minitest'
+
+
 # MonkeyPatch for This version of Rails :(
 if ActionPack::VERSION::STRING >= "5.2.0"
   Minitest::Rails::TestUnit = Rails::TestUnit
@@ -24,6 +28,19 @@ Minitest::Reporters.use!(
   Minitest.backtrace_filter
 )
 
+#configure VCR
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  # config.filter_sensitive_data("<SLACK_TOKEN>") do
+  #   ENV['SLACK_TOKEN']
+  # end
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
